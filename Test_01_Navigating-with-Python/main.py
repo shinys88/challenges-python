5fef7c4b32827cc8a6d714cdd3e9fb0c4f3996d2
shinys88 <sinyusu129@gmail.com>
2021-01-28 11:03:11 +0900

    add Test_01.

---
+++ Test_01_Navigating-with-Python/main.py
@@ -0,0 +1,9 @@
+# import urllib3
+import requests
+# import beautifulsoup as bs
+
+
+indeed_result = requests.get("https://www.indeed.com/jobs?as_and=python&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=25&l=&fromage=any&limit=50&sort=&psf=advsrch&from=advancedsearch")
+
+print(indeed_result)
+
