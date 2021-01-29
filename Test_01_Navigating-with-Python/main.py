0f7d707b611dd24ec60c3b8a769de98c2b1f107c
shinys88 <sinyusu129@gmail.com>
2021-01-29 20:33:15 +0900

    edit Test_01

--- Test_01_Navigating-with-Python/main.py
+++ Test_01_Navigating-with-Python/main.py
@@ -1,9 +1,4 @@
-# import urllib3
-import requests
-# import beautifulsoup as bs
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
-
-indeed_result = requests.get("https://www.indeed.com/jobs?as_and=python&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=25&l=&fromage=any&limit=50&sort=&psf=advsrch&from=advancedsearch")
-
-print(indeed_result)
-
+max_indeed_pages = extract_indeed_pages()
+extract_indeed_jobs(max_indeed_pages)
