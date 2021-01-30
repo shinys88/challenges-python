f59d468247100d30abda16287aeb5d0ac1c438df
shinys88 <sinyusu129@gmail.com>
2021-01-30 12:18:19 +0900

    edit Test_01

--- Test_01_Navigating-with-Python/indeed.py
+++ Test_01_Navigating-with-Python/indeed.py
@@ -6,7 +6,7 @@ LIMIT = 50
 
 URL =f"https://www.indeed.com/jobs?as_and=python&limit={LIMIT}"
 
-def extract_indeed_pages():
+def get_last_page():
   
   result = requests.get(URL)
   soup = BeautifulSoup(result.text, "html.parser")
@@ -23,10 +23,49 @@ def extract_indeed_pages():
   return max_page
 
 
-def extract_indeed_jobs(last_page):
+def extract_job(html):
+
+  title = html.find("h2",{"class":"title"}).find("a")["title"]
+
+  company = html.find("span",{"class":"company"})
+  if company:
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+      company = str(company_anchor.string)
+    else :
+      company = str(company.string)
+    company = company.strip()
+  else:
+    company = None
+
+  location = html.find("div",{"class":"recJobLoc"})['data-rc-loc']
+
+  job_id = html["data-jk"]
+
+  return {'title':title, 
+  'company':company, 
+  'location':location, 
+  'link':f"https://www.indeed.com/cmp/Zachary-Piper-Solutions?fromjk={job_id}"
+  }
+
+
+
+def extract_jobs(last_page):
   jobs = []
   for page in range(last_page):
+    print(f"Scrapping page {page}")
     result = requests.get(f"{URL}&start={page*LIMIT}")
-    print(result.status_code)
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("div",{"class":"jobsearch-SerpJobCard"})
 
+    for result in results :
+      job = extract_job(result)
+      jobs.append(job)
+  return jobs
+
+
+
+def get_jobs():
+  last_page = get_last_page()
+  jobs = extract_jobs(last_page)
   return jobs
--- Test_01_Navigating-with-Python/main.py
+++ Test_01_Navigating-with-Python/main.py
@@ -1,4 +1,5 @@
-from indeed import extract_indeed_pages, extract_indeed_jobs
+from indeed import get_jobs as get_indeed_jobs
+from so import get_jobs as get_so_jobs
 
-max_indeed_pages = extract_indeed_pages()
-extract_indeed_jobs(max_indeed_pages)
+#indeed_jobs = get_indeed_jobs()
+so_jobs = get_so_jobs()
