522150d58062a599720bd8ca9504f41218be9758
shinys88 <sinyusu129@gmail.com>
2021-02-03 23:38:42 +0900

    edit Day_09_10

--- Day_09_10_Api_Flask/main.py
+++ Day_09_10_Api_Flask/main.py
@@ -10,6 +10,7 @@ new = f"{base_url}/search_by_date?tags=story"
 popular = f"{base_url}/search?tags=story"
 
 
+
 # This function makes the URL to get the detail of a storie by id.
 # Heres the documentation: https://hn.algolia.com/api
 def make_detail_url(id):
@@ -17,6 +18,50 @@ def make_detail_url(id):
 
 db = {}
 app = Flask("DayNine")
+
+@app.route("/")
+def main():
+
+  url = popular
+  order_by = request.args.get('order_by')
+
+  if order_by == "new":
+    url = new
+  else :
+    order_by = 'popular'
+
+  # items = None
+
+  existingItems = db.get(order_by)
+  if existingItems:
+    items = existingItems
+  else:
+    result = requests.get(url)
+    result_json = result.json()
+    items = result_json['hits']
+    db[order_by] = items
+    print("main : ",result.status_code)
+
+  return render_template("index.html", items=items, order_by=order_by)
+
+
+@app.route("/<id>")
+def detail(id):
+  # result_json = None
+  existingItems = db.get(id)
+  if existingItems:
+    result_json = existingItems
+  else:
+    url = make_detail_url(id)
+    result = requests.get(url)
+    result_json = result.json()
+    db[id] = result_json
+    print("detail : ",result.status_code)
+    
+
+
+  return render_template("detail.html", result=result_json)
+
 
 
 app.run(host="0.0.0.0")
